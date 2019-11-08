670d5b157da927645a56c3c8e5888a50effd7051
Haxonek <thomash1996@live.com>
2019-11-07 23:30:05 -0700

    Finished proj 2

--- EvenFibonacciNumbers/main.go
+++ EvenFibonacciNumbers/main.go
@@ -4,12 +4,38 @@ import (
     "fmt"
 )
 
-func findFibb(var a uint32, var b uint32) (uint32) {
+func findEvenFibb(a uint32, b uint32) uint32 {
+    var cur uint32 = a + b;
 
+    if cur % 2 == 0 {
+        // even number, return
+        return cur;
+    } else {
+        // odd number, iterate through again
+        if a < b {
+            return findEvenFibb(b, cur)
+        } else {
+            return findEvenFibb(a, cur)
+        }
+    }
 }
 
 func main() {
 
-    fmt.Println("Starting fibonacci sequence")
+    var cur uint32 = 1
+    var last uint32 = 1
+    var MAX_VAL uint32 = 4000000;
+    count := 0
+
+    fmt.Println("Starting fibonacci search")
+    for cur < MAX_VAL {
+        tmp := cur
+        cur = last
+        last = tmp
+        cur = findEvenFibb(cur, last)
+        count++;
+    }
+
+    fmt.Println("There were ",count," even fib numbers, and the last two were ", last, " and ", cur)
 
 }
